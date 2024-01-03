6576bca23825d9c44bd9abaffdcd78cafd0cd521
jem.lee <49692142@qq.com>
2024-01-03 16:16:04 +0800

    Optimize PrivateWebSocketChannelInitializer

--- src/main/java/com/pim/server/netty/PrivateWebSocketChannelInitializer.java
+++ src/main/java/com/pim/server/netty/PrivateWebSocketChannelInitializer.java
@@ -20,12 +20,12 @@ public class PrivateWebSocketChannelInitializer extends ChannelInitializer<Socke
 
     @Override
     protected void initChannel(SocketChannel ch) {
-        ch.pipeline().addLast("logging", new LoggingHandler("DEBUG"));//设置log监听器，并且日志级别为debug，方便观察运行流程
-        ch.pipeline().addLast("http-codec", new HttpServerCodec());//设置解码器
-        ch.pipeline().addLast("aggregator", new HttpObjectAggregator(65536 * 5));//聚合器，使用websocket会用到
-        ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());//用于大数据的分区传输
+        ch.pipeline().addLast("logging", new LoggingHandler("DEBUG"));
+        ch.pipeline().addLast("http-codec", new HttpServerCodec());
+        ch.pipeline().addLast("aggregator", new HttpObjectAggregator(65536 * 5));
+        ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
         ch.pipeline().addLast(new WebSocketServerProtocolHandler("/"));
-        ch.pipeline().addLast("handler", privateWebSocketHandler);//自定义的业务handler
+        ch.pipeline().addLast("handler", privateWebSocketHandler);
         ch.pipeline().addLast(new IdleStateHandler(30, 30, 35, TimeUnit.SECONDS));
         ch.pipeline().addLast(new PrivateServiceHandler());
     }
